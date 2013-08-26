d6284e6d831671334690b2c108ed74db053ef175
Leandro zilli <leandro_zilli_@hotmail.com>
2013-08-26 11:12:42 -0300

    commit inicial

---
+++ App/src/br/com/betha/beans/InicioBesn.java
@@ -0,0 +1,20 @@
+package br.com.betha.beans;
+
+public class InicioBesn {
+private String nome;
+
+
+
+
+
+
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+	
+}
